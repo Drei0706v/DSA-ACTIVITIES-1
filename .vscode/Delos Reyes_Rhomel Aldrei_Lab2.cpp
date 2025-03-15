c8be9548fb7b1e27fe8e3bda21af1b31368e70ec
Delos Reyes, Rhomel Aldrei P. <rd955615@gmail.com>
2025-03-15 15:46:53 +0800

    Update Delos Reyes_Rhomel Aldrei_Lab2.cpp

--- .vscode/Delos Reyes_Rhomel Aldrei_Lab2.cpp
+++ .vscode/Delos Reyes_Rhomel Aldrei_Lab2.cpp
@@ -2,104 +2,183 @@
 using namespace std;
 
 typedef struct Node {
-    string songName;
-    Node *link;
+string songName;
+Node *link;
 } Node;
 
 Node* createNode(string data);
 
-void traverse(Node *head){
-    Node *temp = new Node; //create a temporary node
-    temp = head; // assign temporary node as head
-
-    cout << "My Playlist" <<endl;
-    while(temp != NULL){ //Loop through the nodes
-    cout << temp->songName<< "->" <<endl;
-    if(temp-> link == NULL){ // if tail is reached
-    cout << "NULL"<<endl; // print NULL after
-    }
-    temp = temp->link;
-    }
+void traverse(Node *head) {
+Node *temp = head;
+cout << "My Playlist" << endl;
+while(temp != NULL) {
+cout << temp->songName << "->" << endl;
+if(temp->link == NULL) {
+cout << "NULL" << endl;
+}
+temp = temp->link;
+}
 }
 
-//Create new Node
-    Node *createNode(string data){
-        Node *newNode = new Node; //Create new Node instance
-        newNode->songName = data; //assign data
-        newNode->link = NULL;//assign link
-        
-        return newNode;
-    }
-
-    Node* insertAtEnd(string data, Node* head) {
-    if (head == NULL) { // if the linked list is empty
-        Node* newNode = createNode(data); // create node
-        head = newNode; // assign new node as head
-        cout << "A new node has been inserted at the end \n" << endl; // print out message
-        return head;
-    }
-    
-    Node* temp = head; 
-    
-    while (temp->link != NULL) {
-        temp = temp->link; // move to the next node
-    }
-
-    Node* newNode = createNode(data);
-    temp->link = newNode; // Link the new node to the last element
-
-    cout << "A new node has been inserted at the end \n" << endl;
-    return head;
+Node* createNode(string data) {
+Node *newNode = new Node;
+newNode->songName = data;
+newNode->link = NULL;
+return newNode;
 }
 
-    
-    Node *insertAtBeginning(string data, Node *head){
-        Node *newNode = createNode(data);
-        newNode->link = head;
-        
-        head = newNode;
-        
-        cout << "A new node has been inserted at the beginning \n" <<endl;
-        
-        return head;
-    }
-    
-    string insertAfter(string after, string data, Node *head){
-        Node *temp = new Node;
-        temp = head;
-        
-        while(temp->songName.compare(after) != 0){
-            if(temp == NULL){
-                return "No such song exist, please try again later.";
-            }
-            
-            temp = temp->link;
-            
-        }
-        Node *newNode = createNode(data);
-        newNode->link = temp->link;
-        temp->link = newNode;
-        
-        return "An new node has been added after" + after + "\n";
-    }
+Node* insertAtEnd(string data, Node* head) {
+if (head == NULL) {
+Node* newNode = createNode(data);
+head = newNode;
+return head;
+}
+
+Node* temp = head;
+while (temp->link != NULL) {
+temp = temp->link;
+}
+
+Node* newNode = createNode(data);
+temp->link = newNode;
+return head;
+}
+
+Node* insertAtBeginning(string data, Node *head) {
+Node *newNode = createNode(data);
+newNode->link = head;
+head = newNode;
+return head;
+}
+
+string insertAfter(string after, string data, Node *head) {
+Node *temp = head;
+while(temp != NULL && temp->songName.compare(after) != 0) {
+temp = temp->link;
+}
+
+if (temp == NULL) {
+return "No such song exists, please try again later.";
+}
+
+Node *newNode = createNode(data);
+newNode->link = temp->link;
+temp->link = newNode;
+return "A new node has been added after " + after + "\n";
+}
+
+string deleteAtEnd(Node* head) {
+if (head == NULL) {
+return "The linked list is empty.";
+}
+
+if (head->link == NULL) {
+delete head;
+return "The head has been deleted.";
+}
+
+Node* temp = head;
+while (temp->link->link != NULL) {
+temp = temp->link;
+}
+
+delete temp->link;
+temp->link = NULL;
+return "A node has been deleted at the end \n";
+}
+
+Node* deleteFromBeginning(Node* head) {
+if (head == NULL) {
+return NULL;
+}
+
+Node* temp = head;
+head = head->link;
+delete temp;
+return head;
+}
+
+Node* deleteFromGivenNode(string givenSong, Node* head) {
+if (head == NULL) {
+return NULL;
+}
+
+if (head->songName.compare(givenSong) == 0) {
+head = deleteFromBeginning(head);
+return head;
+}
+
+Node *temp = head;
+while(temp->link != NULL && temp->link->songName.compare(givenSong) != 0) {
+temp = temp->link;
+}
+
+if (temp->link == NULL) {
+return head;
+}
+
+Node* nodeToDelete = temp->link;
+temp->link = temp->link->link;
+delete nodeToDelete;
+return head;
+}
+
 
 
 int main() {
-    Node* head = createNode("Sanctuary by Joji");
-    head = insertAtEnd("Heaven by Bryan Adams", head);
-    head = insertAtEnd("Secret Love Song by Little Mix", head);
-    head = insertAtEnd("I Don't Want to Miss a Thing by Aerosmith", head);
-    
-    head = insertAtBeginning("Ikaw at Ako by Johnoy Danao", head);
-    head = insertAtBeginning("You'll Be Safe Here by Rico Blanco", head);
-    head = insertAtBeginning("Ikaw Parin ang Pipiliin ko by Cup of Joe", head);
-    head = insertAtBeginning("Sunstz by Cigarettes After Sex", head);
+Node* head = createNode("Sanctuary by Joji");
+traverse(head);
+head = insertAtEnd("Heaven by Bryan Adams", head);
+traverse(head);
+head = insertAtEnd("Secret Love Song by Little Mix", head);
+traverse(head);
+head = insertAtEnd("I Don't Want to Miss a Thing by Aerosmith", head);
+traverse(head);
 
-    cout << insertAfter("Ikaw at Ako by Johnoy Danao", "Wag mong aminin by Rico Blanco", head);
-    //cout << insertAfter("You'll Be Safe Here by Rico Blanco","Walang kumakatok by Zild", head);
-    //cout << insertAfter("Ikaw Parin ang Pipiliin ko by Cup of Joe","Ikaw at Ako by Tj Monterde", head);
-    traverse(head);
-    return 0;
+
+
+
+head = insertAtBeginning("Ikaw at Ako by Johnoy Danao", head);
+traverse(head);
+head = insertAtBeginning("You'll Be Safe Here by Rico Blanco", head);
+traverse(head);
+head = insertAtBeginning("Ikaw Parin ang Pipiliin ko by Cup of Joe", head);
+traverse(head);
+head = insertAtBeginning("Sunstz by Cigarettes After Sex", head);
+traverse(head);
+
+
+
+
+cout << insertAfter("Ikaw at Ako by Johnoy Danao", "Wag mong aminin by Rico Blanco", head);
+traverse(head);
+cout << insertAfter("You'll Be Safe Here by Rico Blanco","Walang kumakatok by Zild", head);
+traverse(head);
+cout << insertAfter("Ikaw Parin ang Pipiliin ko by Cup of Joe","Ikaw at Ako by Tj Monterde", head);
+traverse(head);
+
+
+
+cout << "Playlist before deletions:" << endl;
+traverse(head);
+
+cout << "Playlist before deletions:" << endl;
+traverse(head);
+
+cout << "\nDeleting the song at the end (\"Sunstz by Cigarettes After Sex\"):" << endl;
+cout << deleteAtEnd(head) << endl;
+traverse(head);
+
+cout << "\nDeleting the song at the beginning (\"Sanctuary by Joji\"):" << endl;
+head = deleteFromBeginning(head);
+traverse(head);
+
+cout << "\nDeleting a specific song (\"Secret Love Song by Little Mix\" - least favorite):" << endl;
+head = deleteFromGivenNode("Secret Love Song by Little Mix", head);
+traverse(head);
+
+
+return 0;
 }
 
 
